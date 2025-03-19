853960d79a7a80826ed5f2ac0cc638aed0920d80
Hoàng Ngọc Dung <dunghoang0401237@gmail.com>
2025-03-20 03:05:30 +0700

    decoder3to8

---
+++ Chapter 4/IC_Desgin/HoangNgocDung_tb_decoder3to8_en1.v
@@ -0,0 +1,21 @@
+`timescale 1ns/1ns // define timescale
+module dung_tb_decoder3to8_en1();
+
+reg [2:0] in;
+reg en;
+wire [7:0] out;
+
+// initialize w and s to 000 and 00, respectively
+initial begin 
+en = 0; 
+in = 0;
+end
+
+always forever #10 in[0] = ~in[0];
+always forever #20 in[1] = ~in[1];
+always forever #40 in[2] = ~in[2];
+always forever #40 en = ~en;
+
+decoder3to8_en1 m2(in,en,out);
+
+endmodule 
--- Chapter 4/IC_Desgin/HoangNgocDung_test_demux_1o8.v
+++ Chapter 4/IC_Desgin/HoangNgocDung_test_demux_1o8.v
@@ -5,19 +5,18 @@ reg in;
 reg [2:0] sel;
 wire [7:0] out;
 
-// initialize w and s to 000 and 00, respectively
 initial begin 
 in = 1; 
 sel = 0;
 end
 
-// generate s0- w2
+// generate sel0- sel2
 always forever #10 sel[0] = ~sel[0];
 always forever #20 sel[1] = ~sel[1];
 always forever #40 sel[2] = ~sel[2];
 always forever #80 in = in + 1;
 
-// conect w,s,y to module demux8to1
+// conect in,sel,out to module demux8to1
 demux1to8 m1(in,sel,out);
 
 endmodule 
